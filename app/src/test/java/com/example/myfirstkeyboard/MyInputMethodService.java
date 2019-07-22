522655693356893d0b4296818472501e0b3fb199
Robin <robin.blaesing@gmx.de>
2019-07-23 01:28:32 +0200

    First Test

---
+++ app/src/test/java/com/example/myfirstkeyboard/MyInputMethodService.java
@@ -0,0 +1,53 @@
+package com.example.myfirstkeyboard;
+
+import android.inputmethodservice;
+import android.inputmethodservice.KeyboardView;
+
+
+public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+
+    public MyInputMethodService() {
+        super();
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onKey(int i, int[] ints) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void swipeLeft() {
+
+    }
+
+    @Override
+    public void swipeRight() {
+
+    }
+
+    @Override
+    public void swipeDown() {
+
+    }
+
+    @Override
+    public void swipeUp() {
+
+    }
+}
