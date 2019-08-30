e79a1089b7bce0fa083d5a1b3f7e3bb776c51ff9
Robin <robin.blaesing@gmx.de>
2019-08-30 09:40:07 +0200

    Moving keys

--- app/src/main/java/com/example/myfirstkeyboard/KeyMotion.java
+++ app/src/main/java/com/example/myfirstkeyboard/KeyMotion.java
@@ -2,31 +2,12 @@ package com.example.myfirstkeyboard;
 
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.*;
-
-import android.inputmethodservice.KeyboardView;
-import android.inputmethodservice.InputMethodService;
-import android.inputmethodservice.Keyboard;
-import android.text.TextUtils;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.inputmethod.InputConnection;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
-import java.util.*;
 
 public class KeyMotion {
 
     public void newWord(List<Keyboard.Key> word, Keyboard keyboard, KeyboardView keyboardView)  {
-
 
         List<Keyboard.Key> keys = keyboard.getKeys();
 
@@ -43,9 +24,9 @@ public class KeyMotion {
 
             // Check if key already used:
 
-            for (int i = 0; i < codes.length; i++){
+            for (int code: codes){
                 wordWithoutMultiples.add(key);
-                if (key.codes[0] == codes[i]){
+                if (key.codes[0] == code){
                     wordWithoutMultiples.remove(wordWithoutMultiples.size()-1);
                 }
             }
@@ -70,7 +51,6 @@ public class KeyMotion {
             key.x += (int) (Math.signum(centerOfMassX - key.x))*20;
             key.y += (int) (Math.signum(centerOfMassY - key.y))*20;
         }
-
 
     }
 
